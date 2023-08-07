31ebf2ce8e1b684d6d95bef02bc03121dbe8f34b
ronrix <medyochain@gmail.com>
2023-08-07 21:58:27 +0800

    chore: add image placeholder

--- client/components/products/Img.vue
+++ client/components/products/Img.vue
@@ -9,12 +9,21 @@ const props = defineProps<{
 		lazy-src="https://images.unsplash.com/photo-1601892782633-675465fa7f3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80"
 		height="100"
 		cover
+		aspect-ratio="1"
 		style="border-radius: 10px"
 		rounded
 	>
 		<template #placeholder>
 			<div class="d-flex align-center justify-center fill-height">
-				<v-progress-circular color="grey-lighten-4" indeterminate />
+				<!-- <v-progress-circular color="grey-lighten-4" indeterminate /> -->
+				<v-img
+					height="100"
+					cover
+					style="border-radius: 10px"
+					rounded
+					aspect-ratio="1"
+					src="https://images.unsplash.com/photo-1601892782633-675465fa7f3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80"
+				/>
 			</div>
 		</template>
 	</v-img>
